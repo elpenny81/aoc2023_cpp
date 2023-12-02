265dedf9f7545ab7730ac47c9e0d8a08274bd0d0
elpenny <elpenny81dev@gmail.com>
2023-12-02 19:59:34 +0100

    Added clang-format

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -1,12 +1,13 @@
 #include <QDebug>
 #include <QFile>
-#include <QMap>
 #include <QHash>
+#include <QMap>
 #include <QString>
 
 namespace {
 
-QString sum(const QString &fileName, const QHash<QString, QString> &numbersMap) {
+QString sum(const QString &fileName, const QHash<QString, QString> &numbersMap)
+{
     QFile file(fileName);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         qWarning() << "Failed to open file" << fileName;
@@ -15,7 +16,7 @@ QString sum(const QString &fileName, const QHash<QString, QString> &numbersMap) 
 
     quint32 sum = 0;
     while (!file.atEnd()) {
-        const QString line = file.readLine();
+        const QString      line = file.readLine();
         QMap<int, QString> searchResults;
 
         for (QHashIterator i(numbersMap); i.hasNext();) {
@@ -32,50 +33,54 @@ QString sum(const QString &fileName, const QHash<QString, QString> &numbersMap) 
     return QString::number(sum);
 }
 
-QString part1(const QString &inputFile) {
-    return sum(inputFile, {
-        {"0", "0"},
-        {"1", "1"},
-        {"2", "2"},
-        {"3", "3"},
-        {"4", "4"},
-        {"5", "5"},
-        {"6", "6"},
-        {"7", "7"},
-        {"8", "8"},
-        {"9", "9"},
+QString part1(const QString &inputFile)
+{
+    return sum(inputFile,
+               {
+                   {"0", "0"},
+                   {"1", "1"},
+                   {"2", "2"},
+                   {"3", "3"},
+                   {"4", "4"},
+                   {"5", "5"},
+                   {"6", "6"},
+                   {"7", "7"},
+                   {"8", "8"},
+                   {"9", "9"},
     });
 }
 
-QString part2(const QString &inputFile) {
-    return sum(inputFile, {
-        {"zero", "0"},
-        {"one", "1"},
-        {"two", "2"},
-        {"three", "3"},
-        {"four", "4"},
-        {"five", "5"},
-        {"six", "6"},
-        {"seven", "7"},
-        {"eight", "8"},
-        {"nine", "9"},
-        {"0", "0"},
-        {"1", "1"},
-        {"2", "2"},
-        {"3", "3"},
-        {"4", "4"},
-        {"5", "5"},
-        {"6", "6"},
-        {"7", "7"},
-        {"8", "8"},
-        {"9", "9"},
+QString part2(const QString &inputFile)
+{
+    return sum(inputFile,
+               {
+                   {"zero",  "0"},
+                   {"one",   "1"},
+                   {"two",   "2"},
+                   {"three", "3"},
+                   {"four",  "4"},
+                   {"five",  "5"},
+                   {"six",   "6"},
+                   {"seven", "7"},
+                   {"eight", "8"},
+                   {"nine",  "9"},
+                   {"0",     "0"},
+                   {"1",     "1"},
+                   {"2",     "2"},
+                   {"3",     "3"},
+                   {"4",     "4"},
+                   {"5",     "5"},
+                   {"6",     "6"},
+                   {"7",     "7"},
+                   {"8",     "8"},
+                   {"9",     "9"},
     });
 }
 
-}
+} // namespace
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     qInfo() << "Part 1:" << part1(":/input.txt");
     qInfo() << "Part 2:" << part2(":/input.txt");
     return 0;
--- day02/day02.cpp
+++ day02/day02.cpp
@@ -1,35 +1,33 @@
 #include <QDebug>
-#include <QString>
 #include <QFile>
-#include <QObject>
 #include <QMetaEnum>
+#include <QString>
 #include <QVariant>
 
-namespace Day02 
-{
+namespace Day02 {
 
 Q_NAMESPACE
 
-enum class Color {
-    red, green, blue 
-};
+enum class Color { red, green, blue };
 Q_ENUM_NS(Color)
 
-QMap<Color, int> maxScoresFromGame(const QString &game) {
+QMap<Color, int> maxScoresFromGame(const QString &game)
+{
     QMap<Color, int> scores;
     for (const auto &set : game.split(";")) {
         for (const auto &element : set.split(",")) {
-            const auto sep = element.trimmed().split(" ");
+            const auto sep   = element.trimmed().split(" ");
             const auto score = sep[0].toInt();
             const auto color = QVariant::fromValue(sep[1]).value<Color>();
-            scores[color] = qMax(scores[color], score);
+            scores[color]    = qMax(scores[color], score);
         }
     }
     return scores;
 }
 
-template <typename Fn>
-QString process(Fn function, const QString &fileName) {
+template<typename Fn>
+QString process(Fn function, const QString &fileName)
+{
     QFile file(fileName);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         qWarning() << "Failed to open file" << fileName;
@@ -39,44 +37,50 @@ QString process(Fn function, const QString &fileName) {
     quint32 sum = 0;
 
     while (!file.atEnd()) {
-        const QString line = file.readLine();
-        const auto posOfGameSep = line.indexOf(": ");
-        const auto gameId = line.left(posOfGameSep).replace("Game ", "").toInt();
-        const auto game = line.mid(posOfGameSep + 2);
+        const QString line         = file.readLine();
+        const auto    posOfGameSep = line.indexOf(": ");
+        const auto    gameId       = line.left(posOfGameSep).replace("Game ", "").toInt();
+        const auto    game         = line.mid(posOfGameSep + 2);
         sum += function(gameId, game);
     }
 
     return QString::number(sum);
 }
 
-QString part1(const QString &fileName) {
-    const QMap<Color, int> maxScores {
-        {Color::red, 12},
+QString part1(const QString &fileName)
+{
+    const QMap<Color, int> maxScores{
+        {Color::red,   12},
         {Color::green, 13},
-        {Color::blue, 14}
+        {Color::blue,  14}
     };
 
-    return process([&maxScores](quint32 gameId, const QString &game) {
-        const auto scores = maxScoresFromGame(game);
-        if (scores[Color::red] <= maxScores[Color::red] &&
-             scores[Color::green] <= maxScores[Color::green] &&
-             scores[Color::blue] <= maxScores[Color::blue]) {
-            return gameId;
-        }
-        return static_cast<quint32>(0);
-    }, fileName);
+    return process(
+        [&maxScores](quint32 gameId, const QString &game) {
+            if (const auto scores = maxScoresFromGame(game);
+                scores[Color::red] <= maxScores[Color::red] && scores[Color::green] <= maxScores[Color::green]
+                && scores[Color::blue] <= maxScores[Color::blue]) {
+                return gameId;
+            }
+            return static_cast<quint32>(0);
+        },
+        fileName);
 }
 
-QString part2(const QString &fileName) {
-    return process([](quint32, const QString &game) {
-        const auto scores = maxScoresFromGame(game);
-        return scores[Color::red] * scores[Color::green] * scores[Color::blue];
-    }, fileName);
+QString part2(const QString &fileName)
+{
+    return process(
+        [](quint32, const QString &game) {
+            const auto scores = maxScoresFromGame(game);
+            return scores[Color::red] * scores[Color::green] * scores[Color::blue];
+        },
+        fileName);
 }
 
-}
+} // namespace Day02
 
-int main() {
+int main()
+{
     qInfo() << "Part 1:" << Day02::part1(":/input.txt");
     qInfo() << "Part 2:" << Day02::part2(":/input.txt");
     return 0;
